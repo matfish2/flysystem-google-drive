0d69ce93abc956aee829c3b46682d0950ee5d0b6
Matanya <matfish2@gmail.com>
2021-07-27 11:33:50 +0300

    Change supportsTeamDrives to SupportsAllDrives
    https://developers.google.com/resources/api-libraries/documentation/drive/v3/python/latest/drive_v3.files.html
    supportsTeamDrives: boolean, Deprecated use supportsAllDrives instead.

--- src/GoogleDriveAdapter.php
+++ src/GoogleDriveAdapter.php
@@ -1379,7 +1379,7 @@ class GoogleDriveAdapter extends AbstractAdapter
                 'files.copy', 'files.create', 'files.delete',
                 'files.trash', 'files.get', 'files.list', 'files.update',
                 'files.watch'
-            ], ['supportsTeamDrives' => true]),
+            ], ['supportsAllDrives' => true]),
             $this->defaultParams
         );
     }
